c8d0a775afb6962d80527ad0de947c56e8fe2ab9
xkevin190 <kevinvelasco193@gmail.com>
2023-03-27 19:31:58 -0400

    Initial Commit

---
+++ HabitsIOS/HabitsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsIOSApp.swift
+//  HabitsIOS
+//
+//  Created by Kevin Velasco on 27/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
