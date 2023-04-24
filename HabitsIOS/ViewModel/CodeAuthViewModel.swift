f020fb980362b537e2b89fd81d6bb03792e0a16e
xkevin190 <kevinvelasco193@gmail.com>
2023-04-23 20:08:29 -0400

    added the code views and addel the new model

---
+++ HabitsIOS/ViewModel/CodeAuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CodeAuthViewModel.swift
+//  HabitsIOS
+//
+//  Created by Kevin Velasco on 21/4/23.
+//
+
+import Foundation
---
+++ HabitsIOS/Views/SharedComponents/SelectOption.swift
@@ -0,0 +1,20 @@
+//
+//  SelectOption.swift
+//  HabitsIOS
+//
+//  Created by Kevin Velasco on 22/4/23.
+//
+
+import SwiftUI
+
+struct SelectOption: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectOption_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectOption()
+    }
+}
